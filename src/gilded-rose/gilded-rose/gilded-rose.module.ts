f207a46dcca6507f5cfbbba4b87766c245366b15
joren-etw <dev@jlauryssen.com>
2025-11-16 11:29:49 +0100

    import provider in parent module

--- src/gilded-rose/gilded-rose/gilded-rose.module.ts
+++ src/gilded-rose/gilded-rose/gilded-rose.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UpdateItemService } from './services/update-item.service';
+import { RulesProvider } from './services/rules/rules.provider';
 
 @Module({
-  providers: [UpdateItemService],
+  providers: [UpdateItemService, ...RulesProvider],
 })
 export class GildedRoseModule {}
