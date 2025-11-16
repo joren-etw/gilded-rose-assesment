082ad1d604db98fb8d4e09e26b9148cc84822401
joren-etw <dev@jlauryssen.com>
2025-11-16 11:29:32 +0100

    get rule handler in UpdateItemService and apply rule for each item

--- src/gilded-rose/gilded-rose/services/update-item.service.ts
+++ src/gilded-rose/gilded-rose/services/update-item.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { Item } from '../models/item.model';
+import { RuleResolverService } from './rules/rule-resolver.service';
 
 @Injectable()
 export class UpdateItemService {
-  constructor() {}
+  constructor(private readonly resolver: RuleResolverService) {}
 
   handle(items: Item[]): Item[] {
-    return items;
+    return items.map((item) => {
+      const rule = this.resolver.resolve(item.name);
+      return rule.apply(item);
+    });
   }
 }
